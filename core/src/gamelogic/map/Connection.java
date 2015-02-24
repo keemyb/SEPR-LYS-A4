e1c2384d20674d2eddae2315215afdb5884133dd
Akeem Brown <akeemb@gmail.com>
2015-02-24 15:23:50 +0000

    Added connection Type enumeration

--- core/src/gamelogic/map/Connection.java
+++ core/src/gamelogic/map/Connection.java
@@ -19,4 +19,31 @@ public class Connection {
     public Station getStation2() {
         return this.station2;
     }
+
+    public enum ConnectionType {
+        GOLD("Gold", 50, 1),
+        SILVER("Silver", 30, 0.8f),
+        BRONZE("Bronze", 10, 0.5f);
+
+        private String name;
+        private int price;
+        private float hardness;
+        ConnectionType(String name, int price, float hardness) {
+            this.name = name;
+            this.price = price;
+            this.hardness = hardness;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public float getHardness() {
+            return hardness;
+        }
+    }
 }

aaa5517af6157b88f07d0ce7bf4455730cc5a6c4
Akeem Brown <akeemb@gmail.com>
2015-03-01 22:30:32 +0000

    Added methods to modify connections

--- core/src/fvs/taxe/controller/ConnectionController.java
+++ core/src/fvs/taxe/controller/ConnectionController.java
@@ -55,6 +55,66 @@ public class ConnectionController {
         }
     }
 
+    private void createConnection() {
+        if (selectedConnection == null) {
+            context.getTopBarController().displayFlashMessage(
+                    "You have not selected two stations", Color.BLACK, 1000);
+        } else if (map.prospectiveConnectionIsValid(selectedConnection)) {
+            map.addConnection(selectedConnection);
+            PlayerManager.getCurrentPlayer().addOwnedConnection(selectedConnection);
+            PlayerManager.getCurrentPlayer().spendMoney(selectedConnection.calculateCost());
+        } else {
+            context.getTopBarController().displayFlashMessage(
+                    "Cannot Create a Connection Here", Color.BLACK, 1000);
+        }
+    }
+
+    private void upgradeConnection() {
+        if (checkConnectionStatus()) {
+            System.out.println("Upgraded to GOLD");
+            selectedConnection.upgrade(Connection.Material.GOLD);
+            PlayerManager.getCurrentPlayer().spendMoney(selectedConnection.calculateUpgradeCost(Connection.Material.GOLD));
+            clearSelected();
+        }
+    }
+
+    private void repairConnection() {
+        if (checkConnectionStatus()) {
+            System.out.println("Repaired connection");
+            selectedConnection.repair();
+            PlayerManager.getCurrentPlayer().spendMoney(selectedConnection.calculateRepairCost());
+            clearSelected();
+        }
+    }
+
+    private void removeConnection() {
+        if (checkConnectionStatus()) {
+            map.removeConnection(selectedConnection);
+            clearSelected();
+        }
+    }
+
+    public boolean checkConnectionStatus() {
+        if (selectedConnection == null) {
+            context.getTopBarController().displayFlashMessage(
+                    "No connection selected", Color.BLACK, 1000);
+            return false;
+        } else if (selectedConnection.getOwner() == null) {
+            context.getTopBarController().displayFlashMessage(
+                    "Cannot Remove Connection selected", Color.BLACK, 1000);
+            return false;
+        } else if (selectedConnection.getOwner() != PlayerManager.getCurrentPlayer()) {
+            context.getTopBarController().displayFlashMessage(
+                    "You do not own this connection", Color.BLACK, 1000);
+            return false;
+        }
+        return true;
+    }
+
+    public void clearSelected() {
+        selectedStations.clear();
+        selectedConnection = null;
+    }
 
     private void endConnectionModifications() {
         context.getGameLogic().setState(GameState.NORMAL);
